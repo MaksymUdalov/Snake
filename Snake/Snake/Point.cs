89af7646ac3011185ddb5d7a693a40f1ac22c8a7
Max Udalov <udalov.net@gmail.com>
2016-03-20 20:35:05 +0200

    Создан класс Point

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace Snake
+{
+    class Point
+    {
+        int x;
+        int y;
+        char sym;
+
+        public Point(int x, int y, char sym)
+        {
+            this.x = x;
+            this.y = y;
+            this.sym = sym;
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+
+        public override bool Equals(object obj)
+        {
+            Point point = obj as Point;
+            return x == point.x && y == point.y;
+        }
+
+        public override int GetHashCode()
+        {
+            return int.Parse(string.Format("{0}{1}", x, y));
+        }
+
+        public override string ToString()
+        {
+            return string.Format("x = {0}, y = {1}, symbol = {2}",
+                x, y, sym);
+        }
+    }
+}
